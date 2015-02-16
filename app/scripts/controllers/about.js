c7bdc03d12e6c3ab33d50ed05844a1a5cc6e7b58
Ze'ev Gilovitz <zeev.gil@gmail.com>
2015-02-16 21:58:43 +0800

    Yeoman Scaffolding.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gardenTempApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gardenTempApp
+ */
+angular.module('gardenTempApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gardenTempApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gardenTempApp
+ */
+angular.module('gardenTempApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
